3b3ce34f228d4e94671481e13724cd6537d186d8
riccardoimaninoobar <riccardo.imaninoobar@studenti.unicam.it>
2025-03-28 13:06:11 +0100

    ApiRest aggiunto

---
+++ TestAPI/Controllers/Testcontroller.cs
@@ -0,0 +1,30 @@
+﻿using Marche.modelli;
+using Marche.modelli.Modelli;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class Testcontroller : ControllerBase
+    {
+        [HttpGet]
+        public ModelliServiziMarche[] DaiTuttiIServizi()
+        {
+            return FunzioniInterrogazioniServiziMarche.DaiServizi().Result;
+        }
+
+        [HttpGet]
+        public ModelliServiziMarche[] RicercaTuttiIServizi(string Ricerca)
+        {
+            return FunzioniInterrogazioniServiziMarche.RicercaServizi(Ricerca).Result;
+        }
+
+        [HttpGet]
+        public ModelliServiziMarche[] RicercaServiziPerCategoria()
+        {
+            return FunzioniInterrogazioniServiziMarche.RicercaServiziPerCategoria().Result;
+        }
+    }
+}
