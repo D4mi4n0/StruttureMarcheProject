5b609c6e823c78ee75d9092107d5a6e659127ff9
D4mi4n0 <damiano.montanaro@studenti.unicam.it>
2025-04-01 14:10:49 +0200

    Aggiunta cshtml e controller della pagina di ricerca

---
+++ StruttureMarche/Controllers/RicercaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Marche.modelli;
+using Marche.modelli.Modelli;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace StruttureMarche.Controllers
+{
+    public class RicercaController : Controller
+    {
+        public async Task<IActionResult> Index(string? denominazione = "", string? comune = "", string? provincia = "", int page = 1)
+        {
+            int pageSize = 10; // Imposta il numero di risultati per pagina
+
+            // Recupera tutti i risultati senza limitazione
+            var risultati = await FunzioniInterrogazioniServiziMarche.RicercaStrutture(denominazione ?? "", comune ?? "", provincia ?? "");
+
+            // Calcola il numero totale di risultati e il numero di pagine
+            int totalItems = risultati.Length;
+            ViewData["TotalItems"] = totalItems;
+
+            // Limita i risultati in base alla pagina corrente
+            var paginatedResults = risultati.Skip((page - 1) * pageSize).Take(pageSize).ToArray();
+
+            return View(paginatedResults);
+        }
+    }
+}
---
+++ StruttureMarche/Views/Ricerca/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StruttureMarche.Views.Ricerca
+{
+    public class RicercaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
